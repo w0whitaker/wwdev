8b1a1325c422a7c8509c08606847e885dc850a21
William Whitaker <billydwhitaker@gmail.com>
2022-04-21 04:51:41 -0400

    wip--sets up pages

---
+++ src/_data/meta.js
@@ -0,0 +1,8 @@
+module.exports = {
+  url: process.env.URL || "http://localhost:8080",
+  siteName: "William Whitaker",
+  siteDescription:
+    "front-end developer",
+  authorName: "William Whitaker",
+  twitterUsername: "w0whitaker", // no `@`
+};
