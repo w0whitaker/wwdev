9eb2a32019a3bd183b87637272b4e0cdac932fc2
William Whitaker <billydwhitaker@gmail.com>
2022-04-24 12:04:15 -0400

    adds eslint

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,7 @@
-const markdownIt = require("markdown-it");
-const Image = require("@11ty/eleventy-img");
-const path = require("path");
-const classNames = require("classnames");
+const markdownIt = require('markdown-it');
+const Image = require('@11ty/eleventy-img');
+const path = require('path');
+const classNames = require('classnames');
 // const outdent = require("outdent");
 
 const imageShortcode = async (
@@ -9,11 +9,11 @@ const imageShortcode = async (
   alt,
   className,
   widths = [200, 400, 800, 1280],
-  baseFormat = "jpeg",
-  optimizedFormats = ["webp"],
-  sizes = "100vw"
+  baseFormat = 'jpeg',
+  optimizedFormats = ['webp'],
+  sizes = '100vw'
 ) => {
-  const fullSrc = path.join("src", relativeSrc);
+  const fullSrc = path.join('src', relativeSrc);
   const { dir: imgDir } = path.parse(relativeSrc);
   const ImageWidths = {
     ORIGINAL: null,
@@ -22,23 +22,28 @@ const imageShortcode = async (
   const imageMetadata = await Image(fullSrc, {
     widths: [ImageWidths.ORIGINAL, ImageWidths.PLACEHOLDER, ...widths],
     formats: [...optimizedFormats, baseFormat],
-    outputDir: path.join("_site", imgDir),
+    outputDir: path.join('_site', imgDir),
     urlPath: imgDir,
   });
-  const formatSizes = Object.entries(imageMetadata).reduce((formatSizes, [format, images]) => {
-    if (!formatSizes[format]) {
-      const placeholder = images.find((image) => image.width === ImageWidths.PLACEHOLDER);
-      const largestVariant = images[images.length - 1];
+  const formatSizes = Object.entries(imageMetadata).reduce(
+    (formatSizes, [format, images]) => {
+      if (!formatSizes[format]) {
+        const placeholder = images.find(
+          (image) => image.width === ImageWidths.PLACEHOLDER
+        );
+        const largestVariant = images[images.length - 1];
 
-      formatSizes[format] = {
-        placeholder,
-        largest: largestVariant,
-      };
-    }
-    return formatSizes;
-  }, {});
+        formatSizes[format] = {
+          placeholder,
+          largest: largestVariant,
+        };
+      }
+      return formatSizes;
+    },
+    {}
+  );
 
-  const picture = `<picture class="${classNames("lazy-picture", className)}">
+  const picture = `<picture class="${classNames('lazy-picture', className)}">
   ${Object.values(imageMetadata)
     .map((formatEntries) => {
       const { format: formatName, sourceType } = formatEntries[0];
@@ -51,7 +56,7 @@ const imageShortcode = async (
 
       return `<source type="${sourceType}" srcset="${placeholderSrcset}" data-srcset="${actualSrcset}" data-sizes="${sizes}">`;
     })
-    .join("\n")}
+    .join('\n')}
     <img
       src="${formatSizes[baseFormat].placeholder.url}"
       data-src="${formatSizes[baseFormat].largest.url}"
@@ -65,39 +70,38 @@ const imageShortcode = async (
   return picture;
 };
 
-module.exports = function(eleventyConfig) {
+module.exports = function (eleventyConfig) {
   // Copy `src/style.css` to `_site/style.css`
-  eleventyConfig.addPassthroughCopy("src/style.css");
+  eleventyConfig.addPassthroughCopy('src/style.css');
 
   // Custom collections
-  eleventyConfig.addCollection("pages", function(collection) {
-    return collection.getFilteredByGlob("src/pages/*.njk").reverse();
+  eleventyConfig.addCollection('pages', function (collection) {
+    return collection.getFilteredByGlob('src/pages/*.njk').reverse();
   });
-
 
   // Front-matter parsing with gray-matter
   eleventyConfig.setFrontMatterParsingOptions({
     excerpt: true,
-    excerpt_separator: "<!-- excerpt -->"
+    excerpt_separator: '<!-- excerpt -->',
   });
 
   // Parse Markdown properly in excerpts
-  eleventyConfig.addFilter("md", function (content = "") {
+  eleventyConfig.addFilter('md', function (content = '') {
     return markdownIt({ html: true }).render(content);
   });
 
   // Image shortcode https://www.aleksandrhovhannisyan.com/blog/eleventy-image-lazy-loading/
-  eleventyConfig.addNunjucksAsyncShortcode("image", imageShortcode);
+  eleventyConfig.addNunjucksAsyncShortcode('image', imageShortcode);
 
   // Set custom directories for input, output, includes, and data
   return {
     passthroughFileCopy: true,
     dir: {
-      input: "src",
-      includes: "_includes",
-      layouts: "_layouts",
-      data: "_data",
-      output: "_site"
-    }
+      input: 'src',
+      includes: '_includes',
+      layouts: '_layouts',
+      data: '_data',
+      output: '_site',
+    },
   };
 };
--- src/js/index.js
+++ src/js/index.js
@@ -1,32 +1,33 @@
 const lazyLoad = (targets, onIntersection) => {
   const observer = new IntersectionObserver((entries, self) => {
     entries.forEach((entry) => {
-    if (entry.isIntersecting) {
-      onIntersection(entry.target);
-      self.unobserve(entry.target);
-    }
+      if (entry.isIntersecting) {
+        onIntersection(entry.target);
+        self.unobserve(entry.target);
+      }
+    });
   });
-});
-targets.forEach((target) => observer.observe(target));
+  targets.forEach((target) => observer.observe(target));
 };
 
-const lazyPictures = document.querySelectorAll(".lazy-picture");
+const lazyPictures = document.querySelectorAll('.lazy-picture');
 lazyLoad(lazyPictures, (pictureElement) => {
-  const img = pictureElement.querySelector("img");
-  const sources = pictureElement.querySelectorAll("source");
+  const img = pictureElement.querySelector('img');
+  const sources = pictureElement.querySelectorAll('source');
 
   img.onload = () => {
-    pictureElement.dataset.loaded = true;
-    img.removeAttribute("data-src");
+    img.dataset.loaded = true;
+    img.removeAttribute('data-src');
   };
   img.onerror = () => {
-    pictureElement.dataset.loaded = false;
-  }
+    img.dataset.loaded = false;
+  };
   sources.forEach((source) => {
-    source.sizes = source.dataset.sizes;
-    source.srcset = source.dataset.srcset;
-    source.removeAttribute("data-srcset");
-    source.removeAttribute("data-sizes");
+    const imgSrc = source;
+    imgSrc.sizes = source.dataset.sizes;
+    imgSrc.srcset = source.dataset.srcset;
+    source.removeAttribute('data-srcset');
+    source.removeAttribute('data-sizes');
   });
 
   img.src = img.dataset.src;
